aec1737c9de562d8c11225bcce6919e3e7d090a9
joy2323 <joyjoel2011@gmail.com>
2023-03-06 22:42:54 +0100

    fix

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -2,20 +2,56 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Http\Request;
-use Fideloper\Proxy\TrustProxies as Middleware;
 use Illuminate\Contracts\Config\Repository;
+use Fideloper\Proxy\TrustProxies as Middleware;
 
 
 class TrustProxies extends Middleware
 {
+    use Closure;
+
+     /**
+     * The configuration repository instance.
+     *
+     * @var \Illuminate\Contracts\Config\Repository
+     */
+    protected $config;
+
+    /**
+     * Create a new trusted proxies middleware instance.
+     *
+     * @param  \Illuminate\Contracts\Config\Repository  $config
+     * @return void
+     */
+    public function __construct(Repository $config)
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $this->app->set('request', $request);
+    
+        return $next($request);
+    }
+    
+
     /**
      * The trusted proxies for this application.
      *
      * @var array|string|null
      */
 
-    protected $proxies;
+    // protected $proxies;
 
     /**
      * The headers that should be used to detect proxies.
@@ -24,13 +60,13 @@ class TrustProxies extends Middleware
      */
 
     // protected $headers = Request::HEADER_X_FORWARDED_ALL;
-    protected $headers = null;
+    // protected $headers = null;
 
-    public function __construct(Repository $config)
-    {
-        parent::__construct($config->get('trustedproxy.proxies'));
+    // public function __construct(Repository $config)
+    // {
+    //     parent::__construct($config->get('trustedproxy.proxies'));
 
-        $this->headers = $config->get('trustedproxy.headers');
-    }
+    //     $this->headers = $config->get('trustedproxy.headers');
+    // }
 
 }
