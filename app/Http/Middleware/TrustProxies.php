420a24e0bac369db68739c3c656c998e0d60200c
joy2323 <joyjoel2011@gmail.com>
2023-03-09 01:15:58 +0100

    fix

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -2,73 +2,27 @@
 
 namespace App\Http\Middleware;
 
-use Closure;
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
 use Illuminate\Http\Request;
-use Illuminate\Contracts\Config\Repository;
-use Fideloper\Proxy\TrustProxies as Middleware;
-
 
 class TrustProxies extends Middleware
 {
-
-     /**
-     * The configuration repository instance.
-     *
-     * @var \Illuminate\Contracts\Config\Repository
-     */
-    protected $config;
-
-    /**
-     * Create a new trusted proxies middleware instance.
-     *
-     * @param  \Illuminate\Contracts\Config\Repository  $config
-     * @return void
-     */
-    public function __construct(Repository $config)
-    {
-        parent::__construct($config->get('trustedproxy.proxies'));
-
-        $this->headers = $config->get('trustedproxy.headers');
-
-        $this->config = $config;
-    }
-
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, \Closure $next)
-    {
-        return parent::handle($request, $next);
-
-    }
-    
-
     /**
      * The trusted proxies for this application.
      *
-     * @var array|string|null
+     * @var array<int, string>|string|null
      */
-
-    // protected $proxies;
+    protected $proxies;
 
     /**
      * The headers that should be used to detect proxies.
      *
      * @var int
      */
-
-    // protected $headers = Request::HEADER_X_FORWARDED_ALL;
-    // protected $headers = null;
-
-    // public function __construct(Repository $config)
-    // {
-    //     parent::__construct($config->get('trustedproxy.proxies'));
-
-    //     $this->headers = $config->get('trustedproxy.headers');
-    // }
-
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_X_FORWARDED_AWS_ELB;
 }
